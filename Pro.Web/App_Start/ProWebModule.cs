e167ed0ea755ef2d3b7db2d9501b557942bddab6
yzz <1277745281@qq.com>
2017-09-05 16:01:04 +0800

    redis

--- Pro.Web/App_Start/ProWebModule.cs
+++ Pro.Web/App_Start/ProWebModule.cs
@@ -16,62 +16,84 @@ using Microsoft.Owin.Security;
 using Abp.Configuration.Startup;
 using System.Diagnostics;
 using Abp.Localization;
+using System;
+using Abp.Runtime.Caching.Redis;
 
 namespace Pro.Web
 {
-    [DependsOn(
-        typeof(ProDataModule),
-        typeof(ProApplicationModule),
-        typeof(ProWebApiModule),
-        typeof(AbpWebSignalRModule),
-        //typeof(AbpHangfireModule), - ENABLE TO USE HANGFIRE INSTEAD OF DEFAULT JOB MANAGER
-        typeof(AbpWebMvcModule))]
-    public class ProWebModule : AbpModule
-    {
-        public override void PreInitialize()
-        {
-            Debug.WriteLine("------------ ProWebModule PreInitialize");
+	[DependsOn(
+		typeof(ProDataModule),
+		typeof(ProApplicationModule),
+		typeof(ProWebApiModule),
+		typeof(AbpWebSignalRModule),
+		typeof(AbpRedisCacheModule),
+		//typeof(AbpHangfireModule), - ENABLE TO USE HANGFIRE INSTEAD OF DEFAULT JOB MANAGER
+		typeof(AbpWebMvcModule))]
+	public class ProWebModule : AbpModule
+	{
+		public override void PreInitialize()
+		{
+			Debug.WriteLine("------------ ProWebModule PreInitialize");
 
-            //Enable database based localization
-            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+			//Enable database based localization
+			Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
 
-            //Configure navigation/menu
-            Configuration.Navigation.Providers.Add<ProNavigationProvider>();
+			//Configure navigation/menu
+			Configuration.Navigation.Providers.Add<ProNavigationProvider>();
 
-            //Configure Hangfire - ENABLE TO USE HANGFIRE INSTEAD OF DEFAULT JOB MANAGER
-            //Configuration.BackgroundJobs.UseHangfire(configuration =>
-            //{
-            //    configuration.GlobalConfiguration.UseSqlServerStorage("Default");
-            //});
+			//Configure Hangfire - ENABLE TO USE HANGFIRE INSTEAD OF DEFAULT JOB MANAGER
+			//Configuration.BackgroundJobs.UseHangfire(configuration =>
+			//{
+			//    configuration.GlobalConfiguration.UseSqlServerStorage("Default");
+			//});
 
-            Configuration.Modules.AbpWeb().AntiForgery.IsEnabled = false;
-        }
+			Configuration.Modules.AbpWeb().AntiForgery.IsEnabled = false;
 
-        public override void Initialize()
-        {
-            Debug.WriteLine("------------ ProWebModule Initialize");
-            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
 
-            IocManager.IocContainer.Register(
-                Component
-                    .For<IAuthenticationManager>()
-                    .UsingFactoryMethod(() => HttpContext.Current.GetOwinContext().Authentication)
-                    .LifestyleTransient()
-            );
 
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
 
-        public override void PostInitialize()
-        {
-            Debug.WriteLine("------------ ProWebModule PostInitialize");
-        }
+			//Configuration for all caches
+			Configuration.Caching.ConfigureAll(cache =>
+			{
+				cache.DefaultSlidingExpireTime = TimeSpan.FromHours(2);
+			});
 
-        public override void Shutdown()
-        {
-            Debug.WriteLine("------------ ProWebModule Shutdown");
-        }
-    }
+			//Configuration for a specific cache
+			Configuration.Caching.Configure("MyCache", cache =>
+			{
+				cache.DefaultSlidingExpireTime = TimeSpan.FromHours(8);
+			});
+
+			//cache  redis
+			Configuration.Caching.UseRedis();
+
+		}
+
+		public override void Initialize()
+		{
+			Debug.WriteLine("------------ ProWebModule Initialize");
+			IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+			IocManager.IocContainer.Register(
+				Component
+					.For<IAuthenticationManager>()
+					.UsingFactoryMethod(() => HttpContext.Current.GetOwinContext().Authentication)
+					.LifestyleTransient()
+			);
+
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		public override void PostInitialize()
+		{
+			Debug.WriteLine("------------ ProWebModule PostInitialize");
+		}
+
+		public override void Shutdown()
+		{
+			Debug.WriteLine("------------ ProWebModule Shutdown");
+		}
+	}
 }
