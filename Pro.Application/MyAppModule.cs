bfe46251da6f3da323da0878eaf2c1d8f1410faf
yzz <1277745281@qq.com>
2017-09-17 15:43:28 +0800

    -

---
+++ Pro.Application/MyAppModule.cs
@@ -0,0 +1,29 @@
+﻿using Abp.AutoMapper;
+using Abp.Modules;
+using Pro.Authorization.Users;
+using Pro.Persons.Dto;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro
+{
+	[DependsOn(typeof(AbpAutoMapperModule))]
+	public class MyAppModule : AbpModule
+	{
+		public override void PreInitialize()
+		{
+			Debug.WriteLine("------------ MyAppModule PreInitialize");
+
+			Configuration.Modules.AbpAutoMapper().Configurators.Add(config =>
+			{
+				config.CreateMap<PersonDto, User>()
+					  .ForMember(u => u.Password, options => options.Ignore())
+					  .ForMember(u => u.EmailAddress, options => options.MapFrom(input => input.Email));
+			});
+		}
+	}
+}
---
+++ Pro.Application/Persons/Dto/PersonDto.cs
@@ -0,0 +1,22 @@
+﻿using Abp.AutoMapper;
+using Pro.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro.Persons.Dto
+{
+	//[AutoMapTo(typeof(User))]
+	public class PersonDto
+	{
+		public string Name { get; set; }
+
+		public string Surname { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+	}
+}
